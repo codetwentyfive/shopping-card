d9908bf06339d57b5e5d5a10831191aaff77d948
code25 <chingisenkhbaatar@gmail.com>
2024-03-04 15:52:35 +0100

    removed unneeded modetoggle

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "./components/mode-toggle";
 import Router from "./routes/Router";
 import "../app/globals.css";
   
