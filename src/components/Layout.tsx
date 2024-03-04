ff28c1ccfbc272d10587b22d401d88d59273a043
code25 <chingisenkhbaatar@gmail.com>
2024-03-04 20:24:30 +0100

    removed unnecessary css

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow">{children}</main>
+      <main className="">{children}</main>
       <Footer />
     </div>
   );
