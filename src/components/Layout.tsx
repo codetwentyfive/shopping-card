d8325ac4bdddee2009fc297c16d78e7d30362de7
Code25 <chingisenkhbaatar@gmail.com>
2024-03-07 17:11:02 +0100

    passing cart props to header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,11 +4,26 @@ import Footer from "./Footer";
 
 interface LayoutProps {
   children: React.ReactNode;
+  cart: { product: Product; quantity: number }[];
+  totalItems: number;
+  totalPrice: number;
+  removeProduct: (id: number) => void;
 }
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({
+  children,
+  cart,
+  totalItems,
+  totalPrice,
+  removeProduct,
+}) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
+      <Header
+        cart={cart}
+        totalItems={totalItems}
+        totalPrice={totalPrice}
+        removeProduct={removeProduct}
+      />
       <main className="">{children}</main>
       <Footer />
     </div>
