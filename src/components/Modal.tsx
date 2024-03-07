78722967ef8e743fbb22a820383128bcb77eff55
Code25 <chingisenkhbaatar@gmail.com>
2024-03-07 17:12:11 +0100

    adjusted to be uniform across all screens

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -45,10 +45,10 @@ const Modal: React.FC<ModalProps> = ({
 
   return (
     <div
-      className="text-black fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50"
+      className="text-black fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60 z-50"
       onClick={handleOutsideClick}
     >
-      <div className="bg-white p-8 rounded-md max-w-lg">
+      <div className="bg-white p-8 rounded-md max-w-full w-full md:max-w-md">
         <button className="absolute top-2 right-2" onClick={onClose}>
           X
         </button>
@@ -56,7 +56,7 @@ const Modal: React.FC<ModalProps> = ({
           <img
             src={product.image}
             alt={product.name}
-            className="w-48 h-48 object-contain mr-8"
+            className="w-full md:w-48 h-auto object-contain mb-4 md:mb-0 pr-4"
           />
           <div>
             <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
@@ -75,7 +75,7 @@ const Modal: React.FC<ModalProps> = ({
             </div>
             <p className="mb-2">Price: ${product.price}</p>
             <button
-              className="bg-blue-500 text-white px-4 py-2 rounded"
+              className="bg-blue-500 text-white px-4 py-2 rounded "
               onClick={handleAddToCart}
             >
               Add to Bag
